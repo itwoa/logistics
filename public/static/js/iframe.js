5e0dfe884fe27ca86fd1ca251c1c7ee6342d9f18
yanning <635132141@qq.com>
2018-01-27 13:43:28 +0800

    123

---
+++ public/static/js/iframe.js
@@ -0,0 +1,16 @@
+/*左侧导航点击*/
+$(".left_nav a").on("click", function () {
+    $(".left_nav a").removeClass('current');
+    var url = $(this).attr("href");
+    $(this).addClass('current');
+    $(".content").find("iframe").hide().attr("src", url);
+    var index = layer.load();
+
+    $("#iframe").on('load', function () {
+        layer.close(index);
+        $(this).fadeIn();
+    })
+});
+
+
+

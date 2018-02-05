48a89a8219d21f83c09522ccfbfc9cfbc1fe21a2
yanning <635132141@qq.com>
2018-02-05 16:27:09 +0800

    123

--- public/static/js/iframe.js
+++ public/static/js/iframe.js
@@ -2,12 +2,31 @@ $(window).on('load', function () {
 	var height = $(window).outerHeight()-$(".header").height()-40;
 	$("#iframe").css('height',height);
 })
-/*左侧导航点击*/
+
+//导航一级菜单
+$(".left_nav .nav_item").on('click',function(){
+    $('.nav_item').removeClass('current');
+    
+    $(this).addClass('current');
+    $(this).next('ul').toggle(300).siblings('ul').hide();
+
+    if($("i",this).hasClass('fa-caret-down')){
+        $("i",this).removeClass('fa-caret-down').addClass("fa-caret-right");
+    }else{
+        $("i",this).removeClass('fa-caret-right').addClass("fa-caret-down");
+    }
+    
+    $(this).siblings(".nav_item").find("i").removeClass('fa-caret-down').addClass("fa-caret-right");
+
+});
+
+//导航二级菜单
 $(".left_nav a").on("click", function () {
     $(".left_nav a").removeClass('current');
     var url = $(this).attr("href");
     $(this).addClass('current');
     $(".content").find("iframe").hide().attr("src", url);
+    $(this).parents('ul').prev('.nav_item').removeClass("current");
     var index = layer.load();
 
     $("#iframe").on('load', function () {
@@ -15,14 +34,6 @@ $(".left_nav a").on("click", function () {
         $(this).fadeIn();
     })
 });
-
-//二级菜单
-$(".left_nav .nav_item").on('click',function(){
-    $('.nav_item').removeClass('current')
-    $(this).addClass('current');
-    $(this).next('ul').toggle(300).siblings('ul').hide();
-
-})
 
 
 

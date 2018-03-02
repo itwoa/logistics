8d8be03c1cbaed0b582dfdfda264813a78cae648
yanning <635132141@qq.com>
2018-03-02 16:41:41 +0800

    workerman

--- app/index/controller/Home.php
+++ app/index/controller/Home.php
@@ -23,5 +23,30 @@ class Home extends Common
     	$this->assign('id',$id);
     	return view();
     }
+
+    public function sendMsg(){
+
+       
+        $to_uid = "";
+        
+        $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+
+        $push_api_url =  $protocol.$_SERVER['HTTP_HOST'].":2121/";
+        $post_data = array(
+           "type" => "publish",
+           "content" => "这个是推送的测试数据132",
+           "to" => $to_uid, 
+        );
+        $ch = curl_init ();
+        curl_setopt ( $ch, CURLOPT_URL, $push_api_url );
+        curl_setopt ( $ch, CURLOPT_POST, 1 );
+        curl_setopt ( $ch, CURLOPT_HEADER, 0 );
+        curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, 1 );
+        curl_setopt ( $ch, CURLOPT_POSTFIELDS, $post_data );
+        curl_setopt ($ch, CURLOPT_HTTPHEADER, array("Expect:"));
+        $return = curl_exec ( $ch );
+        curl_close ( $ch );
+        var_export($return);
+    }
     
 }
